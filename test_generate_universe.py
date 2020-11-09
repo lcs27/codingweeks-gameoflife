d1fa6554aa1b69923076b6663e51adda17538911
Oo Yue Yang <yueyang.oo@student-cs.fr>
2020-11-09 23:56:00 +0800

    removed error

--- test_generate_universe.py
+++ test_generate_universe.py
@@ -14,9 +14,9 @@ def test_add_seed_to_universe():
     universe = generate_universe(size=(6,6))
     universe = add_seed_to_universe(seed, universe,x_start=1, y_start=1)
     test_equality=np.array(universe ==np.array([[0,0, 0, 0, 0, 0],
- [0, 0, 1, 1, 0, 0],
- [0, 1, 1, 0, 0, 0],
- [0 ,0, 1, 0, 0, 0],
- [0 ,0, 0, 0, 0, 0],
- [0 ,0, 0, 0, 0, 0]],dtype=np.uint8))
+        [0, 0, 1, 1, 0, 0],
+        [0, 1, 1, 0, 0, 0],
+        [0 ,0, 1, 0, 0, 0],
+        [0 ,0, 0, 0, 0, 0],
+        [0 ,0, 0, 0, 0, 0]],dtype=np.uint8))
     assert test_equality.all()
