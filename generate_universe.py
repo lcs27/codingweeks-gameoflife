514f4ba9eaf8248c7403347aa94fb636ec5a24a6
Haonan Lin <lonelvino@gmail.com>
2020-11-09 20:10:08 +0100

    Représentation de l'univers avec une graine positionnée aléatoirement.

--- generate_universe.py
+++ generate_universe.py
@@ -1,8 +1,27 @@
 import numpy as np
 
+#dictionnaire de graines
+seeds = {
+    "boat": [[1, 1, 0], [1, 0, 1], [0, 1, 0]],
+    "r_pentomino": [[0, 1, 1], [1, 1, 0], [0, 1, 0]]
+}
+
+
 def generate_universe(size):
     s = np.zeros(size, dtype=np.int)
     return s
+
+#création d'une graine à partir du dictionnaire de graines
+def create_seed(type_seed):
+    return np.array(seeds[type_seed])
+
+#ajoute la graine dans l'universà la place x_start, y_start
+def add_seed_to_universe(seed,universe, x_start, y_start):
+    (n,p)=np.shape(seed)
+    for i in range(n):
+        for j in range(p):
+            universe[x_start+i,y_start+j]=seed[0+i,0+j]
+    return universe
 
 ### test code
 if __name__ == '__main__':
--- test_generate_universe.py
+++ test_generate_universe.py
@@ -1,6 +1,31 @@
+'''
+Date: 2020-11-09 15:13:17
+LastEditors: Lonel Vino
+LastEditTime: 2020-11-09 20:01:40
+'''
 from generate_universe import *
 from pytest import *
+import numpy as np
+
+# def test_generate_universe():
+#     assert generate_universe((4,4)) == [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+def test_create_seed():
+    seed=create_seed(type_seed = "r_pentomino") 
+    assert (seed == [[0,1,1],[1,1,0],[0,1,0]]).all()
 
 
-def test_generate_universe():
-    assert generate_universe((4,4)) == [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+def test_add_seed_to_universe():
+    seed = create_seed(type_seed="r_pentomino")
+    universe = generate_universe(size=(6, 6))
+    universe = add_seed_to_universe(seed, universe, x_start=1, y_start=1)
+    test_equality = np.array(universe == np.array([[0, 0, 0, 0, 0, 0],
+                                                   [0, 0, 1, 1, 0, 0],
+                                                   [0, 1, 1, 0, 0, 0],
+                                                   [0, 0, 1, 0, 0, 0],
+                                                   [0, 0, 0, 0, 0, 0],
+                                                   [0, 0, 0, 0, 0, 0]], dtype=np.uint8))
+    assert test_equality.all()
+    
+if __name__ == "__main__":
+    test_create_seed()
+    test_add_seed_to_universe()
