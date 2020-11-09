696409e2b65b9da231541ac2fb63535447e64c84
Oo Yue Yang <yueyang.oo@student-cs.fr>
2020-11-09 23:50:02 +0800

    updated functionalite1 tests

--- test_generate_universe.py
+++ test_generate_universe.py
@@ -4,3 +4,19 @@ from pytest import *
 
 def test_generate_universe():
     assert generate_universe((4,4)) == [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+def test_create_seed():
+    seed=create_seed(type_seed = "r_pentomino")
+    assert seed==[[0, 1, 1], [1, 1, 0], [0, 1, 0]]
+
+def test_add_seed_to_universe():
+    seed = create_seed(type_seed = "r_pentomino")
+    universe = generate_universe(size=(6,6))
+    universe = add_seed_to_universe(seed, universe,x_start=1, y_start=1)
+    test_equality=np.array(universe ==np.array([[0,0, 0, 0, 0, 0],
+ [0, 0, 1, 1, 0, 0],
+ [0, 1, 1, 0, 0, 0],
+ [0 ,0, 1, 0, 0, 0],
+ [0 ,0, 0, 0, 0, 0],
+ [0 ,0, 0, 0, 0, 0]],dtype=np.uint8))
+    assert test_equality.all()
