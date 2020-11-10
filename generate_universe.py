19488871be78d30f2583dcf2fedf6bd6ca1896ac
LUO Chensheng <lcchensheng-sofiane.luo@student-cs.fr>
2020-11-10 09:39:42 +0100

    Add new comment

--- generate_universe.py
+++ generate_universe.py
@@ -1,21 +1,78 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def generate_universe(size):
+    """
+    Parameters
+    ----------
+    size :
+        The size of the universe
+
+    Returns
+    -------
+    universe
+        The universe created
+    
+    """ 
     return np.zeros(size, dtype=np.int)
+
 
 dict_seed = {'r_pentomino':[[0, 1, 1], [1, 1, 0], [0, 1, 0]]}
 
 def create_seed(type_seed):
-  return np.array(dict_seed[type_seed])
+    '''
+    Create the seed by the given dictionnary
+
+    Parameters
+    -------
+    type_seed
+        the string of the type of seed
+
+    Returns
+    -------
+    seed
+        the seed obtained as a numpy.array
+
+    '''
+    return np.array(dict_seed[type_seed])
+
 
 def add_seed_to_universe(seed,universe,x_start=0,y_start=0):
+    '''
+    Add the seed to universe by replacing the corresponding places
+
+    Parameters
+    -------
+    seed
+        The numpy.array of the seed to add in
+    universe
+        The numpy.array of the universe to be added
+    x_start
+        The start place of the row parameter to be added(by default 0)
+    y_start
+        The start place of the column parameter to be added(by default 0)
+    
+    Returns
+    -------
+    universe
+        the new universe that has been inserted
+    
+    '''
     num_rows,num_cols = seed.shape
     universe[y_start:y_start+num_rows,x_start:x_start+num_cols] = seed
     return universe
 
 def display_universe(universe):
-    plt.imshow(universe)
+     '''
+     To display the universe
+
+    Parameters:
+    -------
+    universe
+        The np.array of universe to be displayed
+    '''
+    plt.imshow(universe, cmap='Greys')
     plt.show()
 
 if __name__ == "__main__":
--- test_generate_universe.py
+++ test_generate_universe.py
@@ -2,13 +2,16 @@ from generate_universe import *
 from pytest import *
 import numpy as np
 
+
 def test_generate_universe():
     universe = generate_universe((4,4))
     np.testing.assert_array_equal(universe,np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]))
 
+
 def test_create_seed():
     seed = create_seed(type_seed = "r_pentomino")
     np.testing.assert_array_equal(seed,np.array([[0,1,1],[1,1,0],[0,1,0]]))
+
 
 def test_add_seed_to_universe():
     seed = create_seed(type_seed = "r_pentomino")
@@ -22,12 +25,15 @@ def test_add_seed_to_universe():
         [0 ,0, 0, 0, 0, 0]],dtype=np.uint8))
     assert test_equality.all()
 
+
 def test_display_universe():
     test_seed = create_seed(type_seed = "r_pentomino")
     test_universe = generate_universe(size=(6,6))
     test_universe = add_seed_to_universe(test_seed, test_universe,x_start=1, y_start=1)
     display_universe(test_universe)
 
+
+### 1st Layer Test ###
 if __name__ == "__main__":
     test_generate_universe()
     test_create_seed()
