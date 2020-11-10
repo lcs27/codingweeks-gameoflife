1242ed5ea1ef403eb20fb5194308d471d4a276b7
Oo Yue Yang <yueyang.oo@student-cs.fr>
2020-11-10 14:58:26 +0800

    add_seed

--- generate_universe.py
+++ generate_universe.py
@@ -7,6 +7,11 @@ def create_seed(type_seed="r_pentomino"):
     if type_seed == "r_pentomino":
         return np.array([[0,1,1],[1,1,0],[0,1,0]])
 
+def add_seed_to_universe(seed,universe,x_start=0,y_start=0):
+    num_rows,num_cols = seed.shape
+    universe[y_start:y_start+num_rows,x_start:x_start+num_cols] = seed
+    return universe
+
 if __name__ == "__main__":
     #print(generate_universe((4,4)))
     #print(np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]))
