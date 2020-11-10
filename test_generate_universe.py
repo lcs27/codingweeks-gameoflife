a3fad230e44de4f6c36d74ddf1aec3797d461caa
Oo Yue Yang <yueyang.oo@student-cs.fr>
2020-11-10 15:03:16 +0800

    nil

--- test_generate_universe.py
+++ test_generate_universe.py
@@ -8,19 +8,20 @@ def test_generate_universe():
 
 def test_create_seed():
     seed = create_seed(type_seed = "r_pentomino")
+    print(seed)
     np.testing.assert_array_equal(seed,np.array([[0,1,1],[1,1,0],[0,1,0]]))
 
-# def test_add_seed_to_universe():
-#     seed = create_seed(type_seed = "r_pentomino")
-#     universe = generate_universe(size=(6,6))
-#     universe = add_seed_to_universe(seed, universe,x_start=1, y_start=1)
-#     test_equality = np.array(universe == np.array([[0,0, 0, 0, 0, 0],
-#         [0, 0, 1, 1, 0, 0],
-#         [0, 1, 1, 0, 0, 0],
-#         [0 ,0, 1, 0, 0, 0],
-#         [0 ,0, 0, 0, 0, 0],
-#         [0 ,0, 0, 0, 0, 0]],dtype=np.uint8))
-#     assert test_equality.all()
+def test_add_seed_to_universe():
+    seed = create_seed(type_seed = "r_pentomino")
+    universe = generate_universe(size=(6,6))
+    universe = add_seed_to_universe(seed, universe,x_start=1, y_start=1)
+    test_equality = np.array(universe == np.array([[0,0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 0, 0],
+        [0, 1, 1, 0, 0, 0],
+        [0 ,0, 1, 0, 0, 0],
+        [0 ,0, 0, 0, 0, 0],
+        [0 ,0, 0, 0, 0, 0]],dtype=np.uint8))
+    assert test_equality.all()
 
 if __name__ == "__main__":
     test_generate_universe()
