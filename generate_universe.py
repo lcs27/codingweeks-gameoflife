24b9a6e3e9c91233fd0fc309e0d134379da3ef68
Oo Yue Yang <yueyang.oo@student-cs.fr>
2020-11-10 14:41:17 +0800

    create_seed

--- generate_universe.py
+++ generate_universe.py
@@ -3,9 +3,11 @@ import numpy as np
 def generate_universe(size):
     return np.zeros(size, dtype=np.int)
 
-def create_seed(type_seed):
-    pass
+def create_seed(type_seed="r_pentomino"):
+    if type_seed == "r_pentomino":
+        return np.array([[0,1,1],[1,1,0],[0,1,0]])
 
 if __name__ == "__main__":
-    print(generate_universe((4,4)))
-    print(np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]))
+    #print(generate_universe((4,4)))
+    #print(np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]))
+    print(create_seed(type_seed = "r_pentomino"))
