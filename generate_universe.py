098627240dbc70d803ba6da5d1b56cf8df232946
Oo Yue Yang <yueyang.oo@student-cs.fr>
2020-11-10 13:56:31 +0800

    nil

--- generate_universe.py
+++ generate_universe.py
@@ -2,3 +2,7 @@ import numpy as np
 
 def generate_universe(size):
     return np.zeros(size, dtype=np.int)
+
+if __name__ == "__main__":
+    print(generate_universe((4,4)))
+    print(np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]))
