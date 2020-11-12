37a16bbb9d8499536af54dba7991cb70819da5b7
Oo Yue Yang <yueyang.oo@student-cs.fr>
2020-11-12 21:23:35 +0800

    torroidal universe

--- generate_universe.py
+++ generate_universe.py
@@ -19,21 +19,6 @@ def generate_universe(size):
     """ 
     return np.zeros(size, dtype=np.int)
 
-<<<<<<< HEAD
-def create_seed(type_seed="r_pentomino"):
-    if type_seed == "r_pentomino":
-        return np.array([[0,1,1],[1,1,0],[0,1,0]])
-
-def add_seed_to_universe(seed,universe,x_start=0,y_start=0):
-    num_rows,num_cols = seed.shape
-    universe[y_start:y_start+num_rows,x_start:x_start+num_cols] = seed
-    return universe
-
-if __name__ == "__main__":
-    #print(generate_universe((4,4)))
-    #print(np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]))
-    print(create_seed(type_seed = "r_pentomino"))
-=======
 
 def create_seed(type_seed):
     '''
@@ -150,4 +135,3 @@ if __name__ == "__main__":
     #print(np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]))
     #print(create_seed(type_seed = "r_pentomino"))
     pass
->>>>>>> 52d14f8fa3801af8ade4268c6f071e5acee722ef
