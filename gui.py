6834ac3468ee9db30c6e955258e70c1b2ae24db1
Luo  Chensheng Sofiane <chensheng-sofiane.luo@student-cs.fr>
2020-11-13 15:40:56 +0100

    GUI finished

--- gui.py
+++ gui.py
@@ -6,8 +6,10 @@ from functools import partial
 from animation import animate
 from animation import universe_dict
 from generate_universe import *
+from simulation import reset_universe_dict
 from simulation import game_life_simulate
 from dict_seed import dict_seed
+
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,24 +18,15 @@ import time
 CANVAS_HEIGHT = 700
 CANVAS_WIDTH = 700
 NoStop=True
+universe_dict={}
 
-def gui():
-    #Get the root of gui
 
-    tk=Tk()
-    gameoflife=Toplevel(tk)
-    gameoflife.title('Game of life Conway')
-    #animation parties
-    simulation = Canvas(gameoflife,height=CANVAS_HEIGHT,width=CANVAS_WIDTH)
-    #animate(universe_size,seed,seed_position,cmap='Greys',n_generations=30,interval=300,save=False,writer='ffmpeg')
-    
 
-    #Pack each frame
-    simulation.pack()
-    gameoflife.mainloop()
 
 def adapt_universe(tk,canvas,universe,color_line='gray',color_background='white',draw_grill=False):
-
+    '''
+    To create a graphical grid which satis
+    '''
     stop(canvas)
 
     num_rows_univ,num_cols_univ = universe.shape
@@ -57,7 +50,7 @@ def adapt_universe(tk,canvas,universe,color_line='gray',color_background='white'
     tk.update()
     return (dict_lines,dict_rec,WIDTH_OF_CELL)
     
-def display_universe(tk,canvas,universe,graphical_grid,color_background='white',color_foreground='black'):
+def display_universe_canvas(tk,canvas,universe,graphical_grid,color_background='white',color_foreground='black'):
     num_rows_univ,num_cols_univ = universe.shape
     dict_lines=graphical_grid[0]
     dict_rec=graphical_grid[1]
@@ -74,39 +67,50 @@ def display_universe(tk,canvas,universe,graphical_grid,color_background='white',
     tk.update()
     return (dict_lines,dict_rec,WIDTH_OF_CELL)
                 
-def display_animation(tk,canvas,universe,n_generations=None,interval=10,color_line='gray',color_background='white',color_foreground='black',draw_grill=True):
+def display_animation(tk,canvas,universe,n_generations=None,interval=25,color_line='gray',color_background='white',color_foreground='black',draw_grill=True):
     global NoStop
     graphical_grid=adapt_universe(tk,canvas,universe,color_line=color_line,color_background=color_background,draw_grill=draw_grill)
     NoStop=True
-    if n_generations is None:
+    if n_generations is None or n_generations == 0:
         k=0
         while NoStop:
-            graphical_grid=display_universe(tk,canvas,game_life_simulate(universe,k,universe_dict),graphical_grid,color_background=color_background,color_foreground=color_foreground)
+            a=game_life_simulate(universe,k,universe_dict)
+            graphical_grid=display_universe_canvas(tk,canvas,a,graphical_grid,color_background=color_background,color_foreground=color_foreground)
             time.sleep(interval/1000)
             k += 1
     elif isinstance(n_generations,int):
         for k in range(0,n_generations):
-            graphical_grid=display_universe(tk,canvas,game_life_simulate(universe,k,universe_dict),graphical_grid,color_background=color_background,color_foreground=color_foreground)
+            graphical_grid=display_universe_canvas(tk,canvas,game_life_simulate(universe,k,universe_dict),graphical_grid,color_background=color_background,color_foreground=color_foreground)
             time.sleep(interval/1000)
 
-def begin(tk,canvas,universe_x_val,universe_y_val,position_x_val,position_y_val,seed):
+def begin(tk,canvas,universe_x_val,universe_y_val,position_x_val,position_y_val,seed_val,n_generation_val,interval_val):
     global NoStop
     NoStop=True
     try:
-        val_universe_x=int(universe_x_val.get())
-        val_universe_y=int(universe_y_val.get())
-        val_position_x=int(position_x_val.get())
-        val_position_y=int(position_y_val.get())
+        val_universe_x=universe_x_val.get()
+        val_universe_y=universe_y_val.get()
+        val_position_x=position_x_val.get()
+        val_position_y=position_y_val.get()
+        try:
+            val_n_generation=n_generation_val.get()
+        except:
+            val_n_generation=None
+        val_interval=interval_val.get()
     except:
-        messagebox.showwarning('Warning','Only number accept')
-    seed = create_seed(type_seed = seed.get())
-    universe = generate_universe(size=(val_universe_x,val_universe_x))
-    universe = add_seed_to_universe(seed, universe,x_start=val_position_x, y_start=val_position_x)
-    display_animation(tk,canvas,universe)
+        messagebox.showwarning('Warning','Only number accept or size')
 
+    seed = create_seed(type_seed = seed_val.get())
+    universe = generate_universe(size=(val_universe_x,val_universe_y))
+    try:
+        universe = add_seed_to_universe(seed, universe,x_start=val_position_x, y_start=val_position_y)
+    except:
+        messagebox.showwarning('Warning','Size smaller than the seed')
+    display_animation(tk,canvas,universe,n_generations=val_n_generation,interval=val_interval)
+    
 def stop(canvas):
     global NoStop
     NoStop=False
+    reset_universe_dict(universe_dict)
     canvas.delete('all')
 
 def welcome(tk,canvas):
@@ -115,15 +119,25 @@ def welcome(tk,canvas):
     universe = add_seed_to_universe(seed, universe,x_start=10, y_start=0)
     display_animation(tk,canvas,universe)
 
-def destroy(tk):
+def destroy(tk,canvas):
+    stop(canvas)
     tk.destroy()
 
+def change_generation(generation_entry,generation_check_val):
+    generation_entry.delete(0, 'end')
+    if generation_check_val.get() == 0:
+        generation_entry.configure(state=NORMAL)
+    else:
+        generation_entry.configure(state=DISABLED)
+        
 def gui(color_background='white'):
     gameoflife = Tk()
     gameoflife.title('Game of life Conway')
+    
 
     #1.Animation Frames
     simulation = Canvas(gameoflife)
+    gameoflife.protocol('WM_DELETE_WINDOW', partial(destroy,gameoflife,simulation))
 
     #2.Control Frames
     control=Frame(gameoflife,bg=color_background)
@@ -133,8 +147,8 @@ def gui(color_background='white'):
 
     #2.2 Universe entry
     universe_frame=Frame(control)
-    universe_x_val=StringVar(universe_frame)
-    universe_y_val=StringVar(universe_frame)
+    universe_x_val=IntVar(universe_frame)
+    universe_y_val=IntVar(universe_frame)
     universe_x=Entry(universe_frame,textvariable=universe_x_val)
     universe_y=Entry(universe_frame,textvariable=universe_y_val)
     universe_x.grid(row=0,column=0)
@@ -145,8 +159,8 @@ def gui(color_background='white'):
 
     #2.4 Position entry
     position_frame=Frame(control)
-    position_x_val=StringVar(position_frame)
-    position_y_val=StringVar(position_frame)
+    position_x_val=IntVar(position_frame)
+    position_y_val=IntVar(position_frame)
     position_x=Entry(position_frame,textvariable=position_x_val)
     position_y=Entry(position_frame,textvariable=position_y_val)
     position_x.grid(row=0,column=0)
@@ -156,17 +170,34 @@ def gui(color_background='white'):
     seed_label=Label(control,text='Seed',bg=color_background)
 
     #2.6 Seed cblist
-    seed=StringVar(control)
-    seed_cblist=ttk.Combobox(control,textvariable=seed) 
+    seed_val=StringVar(control)
+    seed_cblist=ttk.Combobox(control,textvariable=seed_val) 
     seed_cblist["values"]=list(dict_seed.keys())
-    seed_cblist.current(2)
-
-    #2.7 Begin button
-    begin_button = Button(control,text='Begin',command=partial(begin,gameoflife,simulation,universe_x_val,universe_y_val,position_x_val,position_y_val,seed))
+    seed_cblist.current(0)
     
-    #2.8 Stop button
+    #2.7 generation label
+    generation_label=Label(control,text='Number of generations',bg=color_background)
+
+    #2.8 Number of generations
+    generation_frame=Frame(control)
+    n_generation_val=IntVar(generation_frame)
+    generation_entry=Entry(generation_frame,textvariable=n_generation_val,state=DISABLED,bg=color_background)
+    generation_check_val=IntVar(generation_frame,value=1)
+    generation_check=Checkbutton(generation_frame, text="INFINITE", variable=generation_check_val,command=partial(change_generation,generation_entry,generation_check_val),bg=color_background)
+    generation_check.grid(row=0,column=0)
+    generation_entry.grid(row=0,column=1)
+
+    #2.9 Interval label
+    interval_label=Label(control,text='Interval',bg=color_background)
+
+    #2.10 Interval entry
+    interval_val=IntVar(control,value=25)
+    interval_entry=Entry(control,textvariable=interval_val,bg=color_background)
+
+    #2.7 Begin button,Stop button,Quit button
+    begin_button = Button(control,text='Begin',command=partial(begin,gameoflife,simulation,universe_x_val,universe_y_val,position_x_val,position_y_val,seed_val,n_generation_val,interval_val))
     stop_button = Button(control,text='Stop',command=partial(stop,simulation))
-    quit_button = Button(control,text='Quit',command=partial(destroy,gameoflife))
+    quit_button = Button(control,text='Quit',command=partial(destroy,gameoflife,simulation))
 
     universe_label.grid(row=0,column=0)
     universe_frame.grid(row=1,column=0)
@@ -174,16 +205,20 @@ def gui(color_background='white'):
     position_frame.grid(row=3,column=0)
     seed_label.grid(row=4,column=0)
     seed_cblist.grid(row=5,column=0)
-    begin_button.grid(row=6,column=0)
-    stop_button.grid(row=7,column=0)
-    quit_button.grid(row=8,column=0)
+    generation_label.grid(row=6,column=0)
+    generation_frame.grid(row=7,column=0)
+    interval_label.grid(row=8,column=0)
+    interval_entry.grid(row=9,column=0)
+    begin_button.grid(row=10,column=0)
+    stop_button.grid(row=11,column=0)
+    quit_button.grid(row=12,column=0)
 
     simulation.grid(row=0,column=0,sticky='EWNS')
     control.grid(row=0,column=1,sticky='EWNS')
 
-    #welcome(gameoflife,simulation)
     gameoflife.grid_columnconfigure(0, weight=1)
     gameoflife.grid_rowconfigure(0, weight=1)
+    welcome(gameoflife,simulation)
     gameoflife.mainloop()
 
 
